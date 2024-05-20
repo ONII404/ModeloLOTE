d3b30e95d63a42d254615eafee5179c4f757b534
beehr <beehr@ONNXPC>
2024-05-20 18:11:23 -0500

    add Main Class

--- src/app/main.java
+++ src/app/main.java
@@ -1,8 +1,11 @@
 package app;
 
+import app.ui.Fmain;
+
 public class main {
 
     public static void main(String[] args) {
-        
+        Fmain fmain = new Fmain();
+        fmain.setVisible(true);
     }
 }
