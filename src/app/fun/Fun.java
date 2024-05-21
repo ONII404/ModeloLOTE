ebf79948b053e32e5f0d3c0564f9db8a7ba03c3e
beehr <beehr@ONNXPC>
2024-05-20 22:12:39 -0500

    feat: Add methods to calculate Q and T in Fun.java

--- src/app/fun/Fun.java
+++ src/app/fun/Fun.java
@@ -5,16 +5,34 @@ import java.lang.Math;
 public class Fun {
     
 
+    /**
+     * 
+     * @param S
+     * @param D
+     * @param H
+     * @return
+     */
     Double getQ(double S, double D, double H) {
 
         double Q = (2 * S * D) / H;
         return RaizCuadrada(Q);
     }
 
+    /**
+     * 
+     * @param Q
+     * @param D
+     * @return
+     */
     Double getT(double Q, double D) {
         return Q/D;
     }
 
+    /**
+     * 
+     * @param a
+     * @return
+     */
     Double RaizCuadrada(double a) {
         return Math.sqrt(a);
     }
