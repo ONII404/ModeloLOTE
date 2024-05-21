a611a4983749bb099a0ff405327c0130e03aad29
beehr <beehr@ONNXPC>
2024-05-20 22:12:28 -0500

    feat: Refactor package structure and import statements in GUI.java

--- src/app/ui/GUI.java
+++ src/app/ui/GUI.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-package Frame;
+package app.ui;
 
-/**
- *
- * @author Admin
- */
 public class GUI extends javax.swing.JFrame {
 
     /**
@@ -396,6 +388,9 @@ public class GUI extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
