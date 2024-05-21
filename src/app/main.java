439a65cd8770dc8ec9bb58e8c02f569f9c44decd
LiruFiru <wichonekitokawai@hotmail.com>
2024-05-20 20:03:34 -0500

    Update main.java

--- src/app/main.java
+++ src/app/main.java
@@ -1,12 +1,7 @@
 package app;
 
-import app.ui.Fmain;
+
 
 public class main {
 
-    public static void main(String[] args) {
-
-        Fmain fmain = new Fmain();
-        fmain.setVisible(true);
-    }
 }
